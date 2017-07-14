6908545987b593bc29a256ba5f06c8364ce34cb9
k0s <pop4.ks@gmail.com>
2017-07-14 10:26:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
-#/home/trial420/mysite/flask_app.py
+mport flask
+app = flask.Flask(__name__)
 
-import requests
-from flask import Flask
-app = Flask(__name__)
-
-data = 'hi there'
-r = requests.get('http://cbseresults.nic.in')
-data = r.content
-
-@app.route('/')
-def hello_world():
-    return data	
+@app.route("/")
+def index():
+    #do whatevr here...
+    return "Hello Heruko"
